7733feedf690543ca197f01b7d3dd5d74beee50e
Lars Berge <lb@stud.ntnu.no>
2018-03-07 22:50:18 +0100

    La til funksjonen fillBookRegister() Funskjonen fyller registeret med standard data som gjer testing kjappere. Endret datoene.

--- kiosk/src/kiosk/RegisterOfBooks.java
+++ kiosk/src/kiosk/RegisterOfBooks.java
@@ -171,14 +171,14 @@ public class RegisterOfBooks {
     {
         //Add to single.
         listOfBooks.add(new Book("Fysikk", "Frank", "Pearson", "07.03.2018", 1)); 
-        listOfBooks.add(new Book("Norsk", "Jarl", "Pearson", "24.01.2017", 2));
+        listOfBooks.add(new Book("Norsk", "Jarl", "Norge", "24.01.2017", 2));
         listOfBooks.add(new Book("Matte", "Lars", "Pearson", "07.03.2018", 3));
-        listOfBooks.add(new Book("Naturfag", "Andreas", "Pearon", "07.03.2018", 4));
+        listOfBooks.add(new Book("Naturfag", "Andreas", "Hei", "07.03.2018", 4));
         // Add to series.
-        listOfBooks.add(new Book("Simple","Calclus", "Lars", "Person", "07.03.2018"));
-        listOfBooks.add(new Book("Medium","Calclus", "Jarl", "Person", "07.03.2018"));
-        listOfBooks.add(new Book("Hard","Calclus", "Andreas", "Person", "07.03.2018"));
-        listOfBooks.add(new Book("Advanced","Calclus", "Arne", "Person", "07.03.2018"));
+        listOfBooks.add(new Book("Simple","Calclus", "Lars", "Person", "07.05.2011"));
+        listOfBooks.add(new Book("Medium","Calclus", "Jarl", "Person", "23.11.2013"));
+        listOfBooks.add(new Book("Hard","Calclus", "Andreas", "Person", "13.03.2015"));
+        listOfBooks.add(new Book("Advanced","Calclus", "Arne", "Person", "02.07.2018"));
     }
                                         
 }
