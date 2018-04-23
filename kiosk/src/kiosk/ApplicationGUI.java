78b93ea519553e94bc6dbc9b31cfa621ab1b8de3
Lars Berge <lb@stud.ntnu.no>
2018-04-23 20:46:21 +0200

    Added menu bar and a confirmbox.

--- kiosk/src/kiosk/ApplicationGUI.java
+++ kiosk/src/kiosk/ApplicationGUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kiosk;
 
 import javafx.application.Application;
@@ -10,42 +5,95 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
+ * GUI for the newsstand project.
  *
  * @author berge
+ * 
  */
+
+
+
 public class ApplicationGUI extends Application {
     
+Stage window;
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        
+        window = primaryStage;
+        window.setTitle("Newsstand for OOP group 32.");
+        //Close the program properly:
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        BorderPane root = new BorderPane();        // Create the root node. The Menu will be placed at the top
+        VBox topContainer = new VBox();            //Creates a container to hold all Menu Objects.
+        MenuBar mainMenu = createMenus();          //Creates our main menu to hold our Sub-Menus.
+        // Place the menubar in the topContainer
+        topContainer.getChildren().add(mainMenu);
+        // Place the topcontainer in the top-section of the BorderPane
+        root.setTop(topContainer);
+
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(root, 720, 480);
+
+        window.setScene(scene);
+        window.show();
     }
 
     /**
      * @param args the command line arguments
-    */ 
+     */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
+    /**
+     * Creates the menus to be displayed.
+     */
+    private MenuBar createMenus() {
+        // Create the Menu Bar to hold all the menus
+        MenuBar menuBar = new MenuBar();
+        // The File-menu
+        Menu menuFile = new Menu("File");
+        MenuItem openFile = new MenuItem("Open");
+        openFile.setOnAction(e -> System.out.println("Open Pressed"));
+        
+        MenuItem printFile = new MenuItem("Print");
+        printFile.setOnAction(e -> System.out.println("Print Pressed"));
+        
+        MenuItem exitApp = new MenuItem("Exit");
+        exitApp.setOnAction(e -> closeProgram());
+        
+        menuFile.getItems().addAll(openFile, printFile);
+        menuFile.getItems().add(new SeparatorMenuItem());
+        menuFile.getItems().add(exitApp);
+        // The Edit-menu
+        Menu menuEdit = new Menu("Edit");
+        // The View-menu
+        Menu menuView = new Menu("View");
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+        return menuBar;
+
+    }
+
+    private void closeProgram() {
+        Boolean answer = Confirmbox.display("Confirm","Are you sure you want to exit?");
+        if (answer){
+            window.close();
+        }
+    }
+
 }
